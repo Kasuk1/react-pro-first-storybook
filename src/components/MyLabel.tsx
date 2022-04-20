21582215f90ec72aa351d7230ffd519011eb4a23
Igor Chinchay <ichinchay@sharpminds.com>
2022-04-20 18:16:51 -0500

    First commit with Label Component

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,44 @@
+import './mylabel.css';
+
+export interface IMyLabel {
+  /**
+   * You can type the text in this prop. Be cautious!
+   */
+  label: string;
+  /**
+   * Select the size you want to render.
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * Do you want this label capitalized?.
+   */
+  allCaps?: boolean;
+  /**
+   * Select the contrast of this label.
+   */
+  color?: 'primary' | 'secondary' | 'tertiary';
+  /**
+   * Select the contrast that you want with the color palette
+   */
+  fontColor?: string;
+}
+
+/**
+ * Very first component in StoryBook!
+ */
+export const MyLabel = ({
+  label = 'No label',
+  size = 'normal',
+  allCaps = false,
+  color,
+  fontColor,
+}: IMyLabel) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,47 @@
+import { MyLabel } from '../../components/MyLabel';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'UI/MyLabel',
+  component: MyLabel,
+  argTypes: {
+    size: { control: 'select' },
+    color: { control: 'select' },
+    fontColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof MyLabel>;
+
+const Template: ComponentStory<typeof MyLabel> = (args) => {
+  return <MyLabel {...args} />;
+};
+
+export const Basic = Template.bind({});
+Basic.args = {
+  size: 'normal',
+  allCaps: false,
+  color: 'primary',
+};
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  size: 'normal',
+  allCaps: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  size: 'normal',
+  color: 'secondary',
+};
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  size: 'normal',
+  color: 'tertiary',
+};
+
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+  size: 'h1',
+  fontColor: '#fafafa',
+};
